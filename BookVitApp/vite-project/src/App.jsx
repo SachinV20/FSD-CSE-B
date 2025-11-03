d402119bd4d1d0f9fb6deaac375faf7fd73ef47c
Sachin Verma <vermasachin0208@gmail.com>
2025-11-03 14:06:23 +0530

    for books

---
+++ BookVitApp/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Book from './component/Book'
+function App() {
+  return (
+    <div>
+      <Book/>
+      <Book/>
+      <Book/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ BookVitApp/vite-project/src/component/Book.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../App.css'
+const Book = () => {
+  return (
+    <div className='card'>
+        <img src="" width="200" height="200" alt="image" />
+        <h3>Title: Physics</h3>
+        <h4>Price: ₹671</h4>
+        <button>Add to Cart</button>
+    </div>
+  )
+}
+
+export default Book
